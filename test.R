4a1fb461605b0eb0264dda80d39227f8eb5b5c39
Fredrik Emilsson <fredrik.emilsson@live.se>
2014-05-20 23:27:18 +0200

    Update

---
+++ test.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\Fredrik\\Documents\\R Programming\\Assignments\\Assignment 2\\MineProgrammingAssignment2")
+source("cachematrix.R")
+
+B = matrix(c(2, 4, 3, 1),nrow=2,ncol=2)
+test <- makeCacheMatrix(B)
+test$getinv()
+cache <- cacheSolve(test)
+cache
